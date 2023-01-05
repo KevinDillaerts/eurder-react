ecdbf1f22beefd872acd9650ada7fad49a428b60
KevinDillaerts <kevin.dillaerts@icloud.com>
2023-01-05 09:02:04 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/layout/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/layout/Footer";
+import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import Items from "./components/items/Items";
+import CreateItem from "./components/items/CreateItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <Header/>
+            <main className="container-fluid">
+                <section className="row">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/items" element={<Items />} />
+                            <Route path="/items/create" element={<CreateItem />} />
+                            <Route path="*" element={<Navigate to="/" />} />
+                        </Routes>
+                    </BrowserRouter>
+                    <Outlet />
+                </section>
+            </main>
+        <Footer/>
+        </>
+    );
 }
 
 export default App;
